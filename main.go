901d1020f989a496e767bc6a1b27f57a5d6d90f7
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-06-27 13:31:50 -0400

    Converted to use channels

--- main.go
+++ main.go
@@ -16,40 +16,52 @@ func main() {
 	}
 	defer f.Close()
 
-	// Vars for keeping track of progress in file
-	off := int64(0)
-	curr := ""
-	for {
+	ch := getLinesChannel(f)
 
-		// Buffer to hold the next 8 bytes we read from file
-		buf := make([]byte, 8)
-
-		// Read the next 8 bytes and save to buffer
-		n, err := f.ReadAt(buf, off)
-		isEOF := errors.Is(err, io.EOF) // Detect end of file
-		if err != nil && !isEOF {
-			fmt.Println("Unexpected error reading file:", err)
-			return
-		}
-
-		//tmp := ""
-
-		// Populate our string holding a full line
-		for _, r := range []rune(string(buf)) {
-			if r == '\n' {
-				fmt.Printf("read: %s\n", curr)
-				curr = ""
-			} else {
-				curr += string(r)
-			}
-		}
-
-		// Break from loop once we hit end of file
-		if isEOF {
-			break
-		}
-
-		// Increment byte pointer in file
-		off += int64(n)
+	for st := range ch {
+		fmt.Printf("read: %s\n", st)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	messages := make(chan string)
+
+	go func() {
+		// Vars for keeping track of progress
+		curr := ""
+		for {
+
+			// Buffer to hold the next 8 bytes we read from file
+			buf := make([]byte, 8)
+
+			// Read the next 8 bytes and save to buffer
+			_, err := io.ReadAtLeast(f, buf, 1)
+			isEOF := errors.Is(err, io.EOF) // Detect end of file
+			if err != nil && !isEOF {
+				fmt.Println("Unexpected error reading file:", err)
+				break
+			}
+
+			//tmp := ""
+
+			// Populate our string holding a full line
+			for _, r := range string(buf) {
+				if r == '\n' {
+					messages <- curr
+					curr = ""
+				} else {
+					curr += string(r)
+				}
+			}
+
+			// Break from loop once we hit end of file
+			if isEOF {
+				break
+			}
+
+		}
+
+		close(messages)
+	}()
+	return messages
 }
