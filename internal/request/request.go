8bf14408562099b4085cc8f61282561552ba3d89
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-07-06 19:02:03 -0400

    feat: Implemented request line parsing

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,10 @@
 package request
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 type Request struct {
 	RequestLine RequestLine
@@ -13,13 +17,56 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	_, err := io.ReadAll(reader)
+	b, err := io.ReadAll(reader)
 
-	var req *Request
+	req := Request{}
 
 	if err != nil {
-		return req, err
+		return &req, err
 	}
 
-	return req, nil
+	s := string(b)
+
+	lines := strings.Split(s, "\r\n")
+
+	rl, err := parseRequestLine(lines[0])
+	if err != nil {
+		return &req, err
+	}
+
+	req.RequestLine = *rl
+
+	return &req, nil
+}
+
+func parseRequestLine(line string) (*RequestLine, error) {
+	parts := strings.Split(line, " ")
+
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("request line muct have exactly 3 parts. Received: %v", len(parts))
+	}
+
+	method := parts[0]
+	path := parts[1]
+	version := strings.ReplaceAll(parts[2], "HTTP/", "")
+
+	// Validate method
+	if method != strings.ToUpper(method) {
+		return nil, fmt.Errorf("method must be all caps. Received: %v", method)
+	}
+
+	// Validate path
+
+	// Validate version
+	if version != "1.1" {
+		return nil, fmt.Errorf("only HTTP/1.1 is supported. Received: %v", version)
+	}
+
+	rl := RequestLine{
+		HttpVersion:   version,
+		RequestTarget: path,
+		Method:        method,
+	}
+
+	return &rl, nil
 }
