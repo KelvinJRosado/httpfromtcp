b7ba10a040d773b612cd0a9c958fec9ff6478c21
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-07-05 20:51:08 -0400

    chore: added stubs

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,25 @@
 package request
+
+import "io"
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	_, err := io.ReadAll(reader)
+
+	var req *Request
+
+	if err != nil {
+		return req, err
+	}
+
+	return req, nil
+}
