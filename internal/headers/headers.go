80fb488ccd9165efb5dd4d0006b7eeae1d67ddd3
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-07-12 16:52:09 -0400

    feat: Implemented header parsing

---
+++ internal/headers/headers.go
@@ -0,0 +1,49 @@
+package headers
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Headers map[string]string
+
+const crlf = "\r\n"
+
+func NewHeaders() Headers {
+	return make(Headers)
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	// convert to string for utility
+	rawLine := string(data)
+
+	// If no CRLF present, we need more data
+	if !strings.Contains(rawLine, crlf) {
+		return 0, false, nil
+	}
+
+	// If CRLF is at start, we are finished
+	if strings.HasPrefix(rawLine, crlf) {
+		return 0, true, nil
+	}
+
+	// Only process 1st header (1 at a time)
+	line := strings.Split(rawLine, crlf)[0]
+
+	// Extract the name and value
+	fieldLine := strings.SplitN(line, ":", 2)
+	fieldName := fieldLine[0]
+
+	// Ensure name is valid
+	if strings.HasPrefix(fieldName, " ") || strings.HasSuffix(fieldName, " ") {
+		return 0, false, fmt.Errorf("header name cannot have spaces at start or end. Received: %v", fieldName)
+	}
+
+	// Remove optional whitespace
+	fieldValue := strings.Trim(fieldLine[1], " ")
+
+	// Update map
+	h[fieldName] = fieldValue
+
+	return (len(line) + len(crlf)), false, nil
+}
