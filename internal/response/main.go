4a528b9429c0316a41aa70a01e2881e02e12b338
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-07-20 18:17:54 -0400

    chore: added initial status line write

--- internal/response/main.go
+++ internal/response/main.go
@@ -1,6 +1,8 @@
 package response
 
-import "io"
+import (
+	"io"
+)
 
 type StatusCode int
 
@@ -10,4 +12,19 @@ const (
 	Status500
 )
 
-func WriteStatusLine(w io.Writer, statusCode StatusCode) error
+func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+	switch statusCode {
+	case Status200:
+		_, err := w.Write([]byte("HTTP/1.1 200 OK"))
+		return err
+	case Status400:
+		_, err := w.Write([]byte("HTTP/1.1 400 Bad Request"))
+		return err
+	case Status500:
+		_, err := w.Write([]byte("HTTP/1.1 500 Internal Server Error"))
+		return err
+	default:
+		_, err := w.Write([]byte("HTTP/1.1 500 "))
+		return err
+	}
+}
